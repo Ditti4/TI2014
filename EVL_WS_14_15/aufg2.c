43f74e8f7039460f70c8454edb8c9c98a60b640b
Rico Dittrich <ricod1996@gmail.com>
2015-01-14 11:23:23 +0100

    Fixed ex 2, should work properly now

--- EVL_WS_14_15/aufg2.c
+++ EVL_WS_14_15/aufg2.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-void analyze(char *string) {
+void analyze(unsigned char *string) {
     int max = 0, min = 255, space = -1, i, counter = 0;
 
     for(i = 0; i < 256 && string[i - 1] != '\0'; i++) {
         if(string[i] == ' ') {
-            if(i - 1 >= 0 && string[i - 1] != ' ' )
-	        counter++;
-            if(i - space + 1 < min) {
-                min = i - space + 1;
-            } else if(i - space + 1) {
-                max = i - space + 1;
+            if(i - 1 >= 0 && string[i - 1] != ' ' ) {
+                counter++;
+                if(i - space - 1 < min) {
+                    min = i - space - 1;
+                }
+                if(i - space - 1 > max) {
+                    max = i - space - 1;
+                }
             }
             space = i;
         } else if(i + 1 >= 256 || string[i] == '\0'){
-            counter++;
-            if(i - space + 1 < min) {
-                min = i - space + 1;
-            } else if(i - space + 1) {
-                max = i - space + 1;
+            if(string[i - 1] != ' ') {
+                counter++;
+                if(i - space - 1 < min) {
+                    min = i - space - 1;
+                }
+                if(i - space - 1 > max) {
+                    max = i - space - 1;
+                }
             }
         }
         if(i + 1 < 256 && string[i + 1] == ' ' && string[i] == ' ') {
@@ -29,7 +34,7 @@ void analyze(char *string) {
 }
 
 void main() {
-    char string[256];
+    unsigned char string[256];
 
     gets(string);
     analyze(string);
