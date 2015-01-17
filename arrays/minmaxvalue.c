d100987476e2542bf6de251490952e67a1c6fa23
Rico Dittrich <ricod1996@gmail.com>
2015-01-18 00:29:54 +0100

    Removed the 'MS trying to be super-secure' hack I had in my code

--- arrays/minmaxvalue.c
+++ arrays/minmaxvalue.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#define print(__string) int __i=0; for(__i=0; __string[__i] != '\0'; __i++) putchar(__string[__i]);
-#define scanf scanf_s
+#define print(__string) for(__i=0; __string[__i] != '\0'; __i++) putchar(__string[__i]);
+int __i;
 
 int main() {
 	int numbers[10], max = -32768, min = 32767, i = 0, maxcount = 0, mincount = 0, smincount = 0, smaxcount = 0;
