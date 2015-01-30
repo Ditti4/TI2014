438967c49473b6556331c2e36b35a58e68994fa1
Rico Dittrich <ricod1996@gmail.com>
2015-01-30 14:29:27 +0100

    Add destroyResult() to prevent memory leaks

--- memorymanagement/concat.c
+++ memorymanagement/concat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../result.h"
 
 int str_len(char *string) {
     int i;
@@ -7,13 +8,14 @@ int str_len(char *string) {
     return i;
 }
 
-char *concat(char *string1, char *string2) {
+result *concat(char *string1, char *string2) {
+    PREPARE_RESULT()
     if(!string1 || !string2)
-        return NULL;
+        return setError(1, initResult());
     char *concatedstring;
     int len1 = str_len(string1), len2 = str_len(string2);
     if(!(concatedstring = malloc((len1 + len2 + 2) * sizeof(char))))
-        return NULL;
+        return setError(1, initResult());
     int i;
     for(i = 0; i < len1; i++)
         concatedstring[i] = string1[i];
@@ -21,12 +23,17 @@ char *concat(char *string1, char *string2) {
     for(i = 0; i < len2; i++)
         concatedstring[len1 + i + 1] = string2[i];
     concatedstring[len1 + len2 + 1] = '\0';
-    return concatedstring;
+    return setResult(concatedstring, initResult());    
 }
 
 int main(int argc, char **argv) {
-    char *concated = concat("test", "test2");
-    printf(concated);
-    putchar('\n');
+    result *rp  = concat("test", "test2");
+    if(!rp->errid) {
+        printf(rp->result);
+        putchar('\n');
+    } else {
+        printf("Error: %d", rp->errid);
+        destroyResult(rp);
+    }
     return 0;
 }
