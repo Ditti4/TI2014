2a9bb8c4bb72b0e8f1fa016b49bf2866039022ea
Rico Dittrich <ricod1996@gmail.com>
2015-06-02 18:17:40 +0200

    Remove print stuff from constructor of template

--- alds/templatestuff.cpp
+++ alds/templatestuff.cpp
@@ -6,14 +6,17 @@ using namespace std;
 template <class T>
 class test {
 public:
-    test(T var) {
+    void print_me(T var) {
         cout << var << endl;;
     }
 };
 
 int main(int argc, char **argv) {
-    test<int> intprint(4);
-    test<string> stringprint("test");
-    test<float> floatprint(3.1415926);
+    test<int> intprint;
+    test<string> stringprint;
+    test<float> floatprint;
+    intprint.print_me(4);
+    stringprint.print_me("test");
+    floatprint.print_me(3.1415);
     return 0;
 }
